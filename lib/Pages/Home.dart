e1231d9e691b75e67be0a80ad8f97f8a54c7a0a8
diegosoriarios <diegosoriarios@gmail.com>
2020-03-29 15:08:56 -0300

    Local server

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,10 +1,59 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flwitter/Models/post.dart';
 import 'package:flwitter/Pages/Details.dart';
 import 'package:flwitter/Services/api.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
   final repository = new PostRepository();
+
+  Future _deletePost(post) async {
+    var id = post.sId;
+    var url = "http://localhost:3001/posts/?_id=$id";
+    Response response = await Dio().delete(url);
+    //print(response);
+    Navigator.of(context).pop();
+  }
+
+  void _exibirDialog(post) {
+    Widget cancelaButton = FlatButton(
+      child: Text("Não"),
+      onPressed:  () => Navigator.of(context).pop(),
+    );
+    Widget continuaButton = FlatButton(
+      child: Text("Sim"),
+      onPressed:  () {
+        _deletePost(post);
+      },
+    );
+    //configura o AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("AlertDialog"),
+      content: Text("Deseja excluir post?"),
+      actions: [
+        cancelaButton,
+        continuaButton,
+      ],
+    );
+
+    showDialog(
+      context:  context,
+      builder:  (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +71,9 @@ class HomePage extends StatelessWidget {
               itemCount: posts.length,
               itemBuilder: (context, i) {
                 return ListTile(
+                  onLongPress: (() {
+                    _exibirDialog(posts[i]);
+                  }),
                   title: Text(posts[i].title),
                   subtitle: Text(posts[i].author.name),
                   leading: Image.network(posts[i].author.image),
--- lib/Services/api.dart
+++ lib/Services/api.dart
@@ -1,9 +1,11 @@
 import 'package:flwitter/Models/post.dart';
 import 'package:dio/dio.dart';
+import './data.dart';
 
 class PostRepository {
   Future<List<Post>> getAll() async {
-    var url = "https://api.balta.io/v1/posts";
+    //var url = "https://api.balta.io/v1/posts";
+    var url = "http://localhost:3001/posts?&_limit=10_page=$page";
     Response response = await Dio().get(url);
     return (response.data as List).map((post) => Post.fromJson(post)).toList();
   }
